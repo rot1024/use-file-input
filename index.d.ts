1b7b31c129a0b49c65b246f5688e779a1b6ee4a3
rot1024 <aayhrot@gmail.com>
2019-07-04 14:28:55 +0900

    fix type

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-const useFileInput: (callback: (files: FileList) => void, options: { accept?: string; multiple?: boolean }) => () => void;
+const useFileInput: (callback: (files: FileList) => void, options?: { accept?: string; multiple?: boolean }) => () => void;
 
 export default useFileInput;
